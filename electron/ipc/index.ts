2a60b80183ab677f9f32e95df06b92c654ca87c7
thenethiranbalajis-sys <thenethiranbalajis@gmail.com>
2026-09-22 20:36:22 +0530

    feat: register model and credential IPC handlers

--- electron/ipc/index.ts
+++ electron/ipc/index.ts
@@ -1,5 +1,9 @@
 import { registerAIHandlers } from "./ai";
+import { registerCredentialHandlers } from "./settings";
+import { registerModelHandlers } from "./models";
 
 export function registerIPCHandlers(): void {
   registerAIHandlers();
+  registerCredentialHandlers();
+  registerModelHandlers();
 }
