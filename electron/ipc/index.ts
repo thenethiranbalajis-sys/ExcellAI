75356f4127465ecdf4dacc4de4bb948fa328f061
thenethiranbalajis-sys <thenethiranbalajis@gmail.com>
2026-09-22 21:25:40 +0530

    feat: register conversation IPC handlers

--- electron/ipc/index.ts
+++ electron/ipc/index.ts
@@ -1,9 +1,11 @@
 import { registerAIHandlers } from "./ai";
 import { registerCredentialHandlers } from "./settings";
 import { registerModelHandlers } from "./models";
+import { registerConversationHandlers } from "./conversations";
 
 export function registerIPCHandlers(): void {
   registerAIHandlers();
   registerCredentialHandlers();
   registerModelHandlers();
+  registerConversationHandlers();
 }
